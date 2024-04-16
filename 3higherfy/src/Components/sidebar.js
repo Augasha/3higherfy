e7331e1fa3fd153f97275d0395866539afed0a9a
Augasha <3higher04@gmail.com>
2024-04-16 13:42:25 +0300

    created a new div on the playlist div

--- 3higherfy/src/Components/sidebar.js
+++ 3higherfy/src/Components/sidebar.js
@@ -7,7 +7,9 @@ function Sidebar() {
             <div className='sidebar'>
                 <h1>Playlist</h1>
                 <div className='playlist'>
-                    
+                <h4 class="active"><span></span><i class="bi bi-music-note"></i>Playlist</h4>
+                <h4><span></span><i class="bi bi-music-note"></i>Recently Played</h4>
+                <h4><span></span><i class="bi bi-music-note"></i>Recommended</h4>
                 </div>
             </div>
 
